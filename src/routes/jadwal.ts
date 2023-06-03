946531ae0c8af14e77b30a148223c4ccda295443
Devyan Astagiri <personal.astagiriii@gmail.com>
2023-06-03 14:51:32 +0700

    feat: adding new route getJadwalByDay

---
+++ src/routes/jadwal.ts
@@ -0,0 +1,40 @@
+import { Router, Request } from 'express';
+import { Prisma } from '@prisma/client';
+import prisma from '../utils/prisma';
+
+const JadwalRoute = Router();
+
+// get Jadwal by hari
+JadwalRoute.get(
+  '/getJadwalByDay',
+  async (req: Request<{}, {}, {}, { day: String }>, res) => {
+    const { day } = req.query;
+
+    if (!day) {
+      res.json({
+        message: 'please provide query string: day'
+      });
+    }
+
+    const jadwal = await prisma.jadwalPoli.findMany({
+      where: {
+        hari: day as string | Prisma.StringFilter
+      },
+      select: {
+        hari: true,
+        waktu: true,
+        Poli: {
+          select: {
+            nama: true
+          }
+        }
+      }
+    });
+
+    res.json({
+      data: jadwal
+    });
+  }
+);
+
+export default JadwalRoute;
